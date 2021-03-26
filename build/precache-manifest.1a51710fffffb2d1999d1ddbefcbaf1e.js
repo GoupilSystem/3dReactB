72e00a7f61dc5252691d4946f6f99a921bc82dcb
Scarlett Ihlen <scarleti@uio.no>
2021-03-26 23:46:23 +0100

    try

---
+++ build/precache-manifest.1a51710fffffb2d1999d1ddbefcbaf1e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa76b3383c362ccaca02f4aab3e5c004",
+    "url": "/index.html"
+  },
+  {
+    "revision": "febc66336ca37cb15bdc",
+    "url": "/static/css/main.0e64c1c1.chunk.css"
+  },
+  {
+    "revision": "953a199d2db53309377d",
+    "url": "/static/js/2.ea55e1c9.chunk.js"
+  },
+  {
+    "revision": "febc66336ca37cb15bdc",
+    "url": "/static/js/main.a2d34273.chunk.js"
+  },
+  {
+    "revision": "111e02bda956e2be247d",
+    "url": "/static/js/runtime-main.b62ef695.js"
+  }
+]);
